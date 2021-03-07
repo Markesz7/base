15f1490056ed396e4f7cdf0422a94d875c15da6d
Márk Kaposvári <xmarko45@gmail.com>
2021-03-07 19:27:14 +0100

    Add Tacograph module

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.train.tachograph;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import java.time.LocalTime;
+
+public class TrainTachograph {
+
+    private Table<LocalTime, Integer, Integer> tachograph;
+
+    public TrainTachograph()
+    {
+        tachograph = HashBasedTable.create();
+    }
+
+    public void add(LocalTime currentTime, int joystickPos, int speed)
+    {
+        tachograph.put(currentTime, joystickPos, speed);
+    }
+}
