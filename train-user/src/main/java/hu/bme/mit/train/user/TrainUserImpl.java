74b7104258240455092787d8f81658b3c2949209
Márk Kaposvári <xmarko45@gmail.com>
2021-03-28 01:08:08 +0100

    Add timer for followSpeed function

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -3,10 +3,14 @@ package hu.bme.mit.train.user;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private boolean timer_exists = false;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
@@ -30,8 +34,21 @@ public class TrainUserImpl implements TrainUser {
 
 	@Override
 	public void overrideJoystickPosition(int joystickPosition) {
+
 		this.joystickPosition = joystickPosition;
 		controller.setJoystickPosition(joystickPosition);
-	}
 
+		if(!timer_exists)
+		{
+			Timer timer = new Timer();
+			TimerTask tt = new TimerTask() {
+				@Override
+				public void run() {
+					controller.followSpeed();
+				}
+			};
+			timer.scheduleAtFixedRate(tt, 0, 1000);
+			timer_exists = true;
+		}
+	}
 }
